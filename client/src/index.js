a4421c5404792161aa77907bf53cc4f703191848
Kevin <sheng.kevin184@gmail.com>
2021-12-22 12:06:30 -0500

    implement webpack on fec

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './client/src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './client/dist'
+    }
+  }
+};
+
+module.exports = config;
