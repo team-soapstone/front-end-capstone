91298aafc2cdf1fe5a8367bcd47082076fc76e52
Kevin <sheng.kevin184@gmail.com>
2021-12-29 16:07:39 -0500

    add enzyme test for rating and reviews

--- __tests__/RatingsAndReviews.test.js
+++ __tests__/RatingsAndReviews.test.js
@@ -1,44 +1,39 @@
 import React from 'react';
 import axios from 'axios';
+import { shallow, mount } from 'enzyme';
+
 import RatingsAndReviews from '../client/src/components/RatingsAndReviews/RatingsAndReviews.jsx'
-
-import { create, act } from 'react-test-renderer';
-import { cleanup, screen, render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
+import API_KEY from '../config.js';
 
 jest.mock('axios');
 
-describe('Ratings and Reviews tests', () => {
-  const fakeProduct = {
-    id: '10',
-    name: 'dark hoodie',
-    slogan: 'leet',
-    description: 'This will make you feel like a hacker!',
-    category: 'Sweater',
-    default_price: '50'
-  }
-  const fakeReviews = {
-    product_id: '10',
-    ratings: { 1: '0', 2: '1', 3:'3', 4:'5', 5:'1' },
-    recommended: {},
-    characteristics: {}
-  }
+const fakeProduct = {
+  id: '10',
+  name: 'dark hoodie',
+  slogan: 'leet',
+  description: 'This will make you feel like a hacker!',
+  category: 'Sweater',
+  default_price: '50'
+}
+const fakeReviews = {
+  product_id: '10',
+  ratings: { 1: '0', 2: '1', 3:'3', 4:'5', 5:'1' },
+  recommended: {},
+  characteristics: {}
+}
+const mockUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-nyc/reviews';
 
-  let root;
-  it('should receive the current selected product as a prop from App', () => {
-    act(() => {
-      root = create(<RatingsAndReviews currentProduct={fakeProduct} productRatings={fakeReviews}/>, container);
+describe('Ratings and Reviews', () => {
+  it('should make an API call when a different prop is passed down', () => {
+    axios.get.mockResolvedValue({ data: fakeProduct});
+    const wrapper = shallow(<RatingsAndReviews />);
+
+    expect(axios.get).not.toHaveBeenCalled();
+
+    wrapper.setProps({ currentProduct: fakeProduct })
+    expect(axios.get).toHaveBeenCalledWith(mockUrl, {
+      headers: { Authorization: API_KEY },
+      params: { product_id: fakeProduct.id }
     });
-    console.log(root.toJSON());
-
   })
-
-  // test('should make a GET request to the API after receiving the prop from App', async () => {
-
-  // })
-
-  // test('should make another GET request to the API if receiving a new prop from App', () => {
-
-  // })
 })
