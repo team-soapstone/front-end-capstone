7fdc70747748ba879e93bcafb547f634787a8e64
Kye <kyelindholm@gmail.com>
2021-12-31 18:31:17 -0500

    refactored to funtional component

--- client/src/components/QA/QuestionSearch.jsx
+++ client/src/components/QA/QuestionSearch.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
 
-class QuestionSearch extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(e) {
-    this.props.onSearch(e.target.value);
-  }
-
-
-  render() {
-    return (
-      <div>
-        <input type='text' placeholder='Have a question? Search for answers...' onChange={this.onChange}/>
-      </div>
-    );
-  }
+const QuestionSearch = (props) => {
+  return (
+    <div>
+      <input type='text' placeholder='Have a question? Search for answers...' onChange={(e) => { props.onSearch(e.target.value); }}/>
+    </div>
+  );
 }
 
 export default QuestionSearch;
