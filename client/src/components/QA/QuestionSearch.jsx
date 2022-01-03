157ca1cee7548447a1010d28ffff2e17b803676f
Kye <kyelindholm@gmail.com>
2022-01-03 11:57:11 -0500

    added classNames to a bunch of components and did some initial styling

--- client/src/components/QA/QuestionSearch.jsx
+++ client/src/components/QA/QuestionSearch.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const QuestionSearch = (props) => {
   return (
     <div>
-      <input type='text' placeholder='Have a question? Search for answers...' onChange={(e) => { props.onSearch(e.target.value); }}/>
+      <input className="qaSearch" type='text' placeholder='Have a question? Search for answers...' onChange={(e) => { props.onSearch(e.target.value); }}/>
     </div>
   );
 }
