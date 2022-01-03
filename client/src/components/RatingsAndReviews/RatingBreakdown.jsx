10eb1218a61403756c2e7e9912e16bb65ad78acc
Kevin <sheng.kevin184@gmail.com>
2022-01-03 13:07:34 -0500

    more styling

--- client/src/components/RatingsAndReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsAndReviews/RatingBreakdown.jsx
@@ -10,34 +10,36 @@ const RatingBreakdown = ({ productRatings }) => {
 // color scheme - green and grey
   console.log('Product Ratings ', productRatings)
   const averageRating = Number(ratingAverage(productRatings.ratings));
+  const percentRecommended = Math.round((Number(productRatings.recommended.true) / (Number(productRatings.recommended.false) + Number(productRatings.recommended.true)) * 100));
 
   return (
     <div>
       <div>
         <h2>{averageRating}</h2>
         <Ratings rating={averageRating}/>
+        <p>{percentRecommended}% of reviews recommend this product</p>
       </div>
       <table className='ratingTable'>
         <tbody>
           <tr className='fiveStarRow'>
             <td><button>5 star</button></td>
-            <td><h4>rating bar</h4></td>
+            <td className='barContainer'><div id='fiveStarBar' className='ratingBar' style={{width: '50%'}}>Rating Bar</div></td>
           </tr>
           <tr className='fourStarRow'>
             <td><button>4 star</button></td>
-            <td><h4>rating bar</h4></td>
+            <td className='barContainer'><div id='fourStarBar' className='ratingBar' style={{width: '50%'}}>Rating Bar</div></td>
           </tr>
           <tr className='threeStarRow'>
             <td><button>3 star</button></td>
-            <td><h4>rating bar</h4></td>
+            <td className='barContainer'><div id='threeStarBar' className='ratingBar' style={{width: '50%'}}>Rating Bar</div></td>
           </tr>
           <tr className='twoStarRow'>
             <td><button>2 star</button></td>
-            <td><h4>rating bar</h4></td>
+            <td className='barContainer'><div id='twoStarBar' className='ratingBar' style={{width: '50%'}}>Rating Bar</div></td>
           </tr>
           <tr className='oneStarRow'>
             <td><button>1 star</button></td>
-            <td><h4>rating bar</h4></td>
+            <td className='barContainer'><div id='oneStarBar' className='ratingBar' style={{width: '50%'}}>Rating Bar</div></td>
           </tr>
         </tbody>
       </table>
