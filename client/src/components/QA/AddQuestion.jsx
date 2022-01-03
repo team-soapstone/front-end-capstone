6062558cc453023db017fa180b387f479a2388c7
Kye <kyelindholm@gmail.com>
2022-01-03 12:31:46 -0500

    changed font color for readability

--- client/src/components/QA/AddQuestion.jsx
+++ client/src/components/QA/AddQuestion.jsx
@@ -50,7 +50,7 @@ class AddQuestion extends React.Component {
         <h3 className="addQuestionTitle">Add Question</h3>
         <textarea className="addQuestionInput" placeholder="Your question..." onChange={this.onChangeQuestion} value={this.state.question}/>
         <input className="addQuestionInput" type="text" placeholder="Nickname..." value={this.state.nickname} onChange={this.onChangeNickname}/>
-        <p className="addQuestionInput" style={{ fontSize: "8px", marginTop: '0' }}>For privacy reasons, do not use your full name or email address within nickname.</p>
+        <p className="addQuestionInput" style={{ fontSize: "8px", marginTop: '0', color: 'white' }}>For privacy reasons, do not use your full name or email address within nickname.</p>
         <input className="addQuestionInput" type="email"  placeholder="Email..." value={this.state.email} onChange={this.onChangeEmail}/>
         <input className="addQuestionInput" id="addQuestionSubmit" type="submit"/>
       </form>
